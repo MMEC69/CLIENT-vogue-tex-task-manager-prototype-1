813be9f42f772be9c8c1a68cb7c0bbff73ad335c
MMEC69 <coorayeronnemanoshapineapple@gmail.com>
2024-04-30 02:05:57 +0530

    now it removes by using post with req body

--- src/Components/SingleProjectView/SingleProjectView.jsx
+++ src/Components/SingleProjectView/SingleProjectView.jsx
@@ -36,55 +36,59 @@ export default function SingleProjectView({project}) {
   const deleteProject = async (e) => {
     e.preventDefault();
     try {
-      const {data} = await axios.delete(
+      //To send req.body, put used instead of delete
+      const {data} = await axios.put(
         `/deleteTheProject/${projectName}`, 
-        //isssue here, req body dont parse <-CHECK THIS
         {
           data: projectDeleter
         }
       );
 
-      console.log("fff");
       if(data.error){
         console.log(data.error);
         // return toast.error(data.error);
       }else{
-        // return toast.success("Project Deleted");
         console.log("Project Deleted");
+        // return toast.success("Project Deleted");
       }
 
     } catch (error) {
-      return toast.error(error);
+      console.log("Project didn't deleted");
+      // return toast.error(error);
     }
   }
 
   return (
     <div className='single-project'>
+      <div>
         <div className='project-name'>
-          <p>{project.projectName}</p>
-        </div>
+            <p>{project.projectName}</p>
+          </div>
 
-        <div className='time'>
-          <p>{project.startDate}<span> to </span>{project.dueDate}</p>
-        </div>
+          <div className='time'>
+            <p>{project.startDate}<span> to </span>{project.dueDate}</p>
+          </div>
 
-        <div className='state'>
-          <p>State {project.projectState}</p>
-        </div>
-        
-        <div className='modify'>
-          <button onClick={projectModify}>Modify</button>
-        </div>
+          <div className='state'>
+            <p>State {project.projectState}</p>
+          </div>
+          
+      </div>
 
-        <div className='modify'>
+      <div>
+        <div className='option-button'>
+              <button onClick={projectModify}>Modify</button>
+            </div>
+        <div className='option-button'>
           <button onClick={viewProject}>View</button>
         </div>
 
-        <div className='modify'>
+        <div className='option-button'>
           <form onSubmit={deleteProject}>
             <button type='submit'>Remove</button>
           </form>
         </div>
+      </div>
 
     </div>
   );
